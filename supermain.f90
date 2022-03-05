5adcfa54ed86fcc778b58c8b6c8d71d9de89b656
Neal <nealak@rogers.com>
2022-03-04 19:06:15 -0500

    update range

--- supermain.f90
+++ supermain.f90
@@ -64,11 +64,11 @@
      print*, "Running coupling constant: ", cplConsts(cpl)
      do i = 1,1!1
        dm_Mass = 1.d0!10**(dble(i-1)/5.)
-       do j = 1,101
+       do j = 1,1001
         ! dm_Vel = 10**(dble(j-1)/10. + 1.) ! chris' notes test the range of velocities from 10^8 to 10^9 cm s^-1 for SI xSec
         ! dm_Vel = 4.32d3*(dble(j)*0.0005 + 1.)
         ! dm_Vel = 1.d3 * (dble(j-1)/100. + 4.)
-        dm_Vel = 4300 + dble(j-1) * (4550-4300)/100
+        dm_Vel = 4320. + dble(j-1) * (4495.-4320.)/1000.
         call supercaptn(dm_Mass, dm_Spin, dm_Vel, num_isotopes, dm_Scattered)
         write(55,*) dm_Mass, dm_Vel, dm_Scattered
         ! Use this to check for negative scattering numbers (couplings 5, 7, 8, 13, 14)
