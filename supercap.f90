bc8ba6d3435a25e504b5d2bf44b5f33d1aa4336c
Neal <nealak@rogers.com>
2022-03-17 15:46:58 -0400

    edited kinematic conditional to be simpler computationally

--- supercap.f90
+++ supercap.f90
@@ -302,7 +302,8 @@ subroutine supercaptn(mx_in, jx_in, vel_in, niso, scattered)
 
             ! condition on the maximal energy the DM can get from scattering off the SNe as given by Chris
             ! 1/2 * m_A * V_s^2 * 4 * m_A*m_x/(m_A+m_x)^2
-            if ((0.5*mdm*vel**2 .lt. 2*A**2 * mnuc**2 * mdm/(A*mnuc+mdm)**2*V_s**2)) then
+            ! if ((0.5*mdm*vel**2 .lt. 2*A**2 * mnuc**2 * mdm/(A*mnuc+mdm)**2*V_s**2)) then
+            if (vel .lt. (2*A*mnuc*V_s)/(A*mnuc + mdm)) then
 
                 do w_pow=1,2
 
