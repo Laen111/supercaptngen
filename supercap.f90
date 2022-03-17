e58ec0642876a8f8355cd626f94aa6b02ca68b02
Neal <nealak@rogers.com>
2022-03-17 15:48:36 -0400

    fixed the powers of q and v to be even

--- supercap.f90
+++ supercap.f90
@@ -313,9 +313,9 @@ subroutine supercaptn(mx_in, jx_in, vel_in, niso, scattered)
                             ! the momentum transfer is defined using the energy of moving DM: E = q^2/(mdm*2)
                             ! gives: q = mdm * w
                             if ( eli .eq. 1 ) then ! we're doing Hydrogen which doesn't get an exponential in its W function fits
-                                result = result + prefactor_array(eli,q_pow,w_pow) * (mdm*vel)**(q_pow-1) * (V_s)**(w_pow-1)
+                                result = result + prefactor_array(eli,q_pow,w_pow) *(mdm*vel)**(2*(q_pow-1)) *(V_s)**(2*(w_pow-1))
                             else
-                                result = result + prefactor_array(eli,q_pow,w_pow) * (mdm*vel)**(q_pow-1) * (V_s)**(w_pow-1) &
+                                result = result + prefactor_array(eli,q_pow,w_pow) *(mdm*vel)**(2*(q_pow-1)) *(V_s)**(2*(w_pow-1)) &
                                     * exp(-2*yConverse_array_super(eli)*(mdm*vel)**2)
                             end if
                         end if
